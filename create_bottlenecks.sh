ca1be05aa64ebae7c7a6264e902bc573b29b9de8
Cesare Montresor <cesare.montresor@gmail.com>
2017-04-09 17:30:27 +0200

    paths :(

--- create_bottlenecks.sh
+++ create_bottlenecks.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+export CUDA_HOME=/usr/local/cuda
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
+export PATH="$CUDA_HOME/bin:$PATH"
+
+export DYLD_LIBRARY_PATH="$CUDA_HOME/lib"
+export DYLD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib:$DYLD_LIBRARY_PATH
+
 # echo "Running Inception Cifar10"
 # python run_bottleneck.py --network inception --batch_size 32 --dataset cifar10
 # echo "Running Inception Traffic"
@@ -10,9 +19,9 @@
 # python run_bottleneck.py --network vgg --batch_size 16 --dataset cifar10
 # echo "Running VGG Traffic"
 # python run_bottleneck.py --network vgg --batch_size 16 --dataset traffic
-python shrink.py --network vgg --dataset traffic --size 100
-python shrink.py --network vgg --dataset cifar10 --size 100
-python shrink.py --network resnet --dataset traffic --size 100
-python shrink.py --network resnet --dataset cifar10 --size 100
-python shrink.py --network inception --dataset traffic --size 100
-python shrink.py --network inception --dataset cifar10 --size 100
+python3 shrink.py --network vgg --dataset traffic --size 100
+python3 shrink.py --network vgg --dataset cifar10 --size 100
+python3 shrink.py --network resnet --dataset traffic --size 100
+python3 shrink.py --network resnet --dataset cifar10 --size 100
+python3 shrink.py --network inception --dataset traffic --size 100
+python3 shrink.py --network inception --dataset cifar10 --size 100
